56f239c3d6e4b19b3c56cdebc310cb63d6b46428
Brem0317 <patrickbarcelo17@gmail.com>
2026-01-23 12:48:46 +0800

    setup of the template

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Livewire\HomePage;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/admin');
+Route::redirect('/admin', '/admin');
+
+Route::get('/', HomePage::class);
