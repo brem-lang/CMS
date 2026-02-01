933b1c7cfd1f733c7c5082374b6b53fb7ffa8d24
Sydney Ricafort <51870127+devsyd11@users.noreply.github.com>
2026-02-01 22:37:33 +0800

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,9 +18,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Crist Briand',
+            'email' => 'cboncada@gmail.com',
             'role' => 'admin',
+            'password'=> bcrypt('V9r$7kL!p2Qx@wF8')
         ]);
     }
 }
