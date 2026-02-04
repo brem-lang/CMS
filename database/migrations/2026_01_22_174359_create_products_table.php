1a1591b1537f5f6efc63c244a282b4f0b91e08d2
Brem0317 <patrickbarcelo17@gmail.com>
2026-02-04 12:07:17 +0800

    updates

--- database/migrations/2026_01_22_174359_create_products_table.php
+++ database/migrations/2026_01_22_174359_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->text('description');
             $table->foreignId('added_by')->constrained('users')->onDelete('cascade');
             $table->decimal('price', 10, 2);
             $table->integer('stock_quantity');
